daf98d58b73c9d6752dc8dd4e169f5436d483756
Jhon Rueda <jrueda@beesion.com>
2023-12-20 04:14:11 +0100

    Adding changes

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from src import create_app
+import os
 
+port = int(os.environ.get("PORT", 5000))
 app = create_app()
 print("folder", app.config['BASE_PATH'])
 if __name__ == "__main__":
     app.run(
         debug=True,
         host='0.0.0.0',
-        port=5000
+        port=port
     )
